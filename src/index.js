e8563b9706c36644a72f7d64592a0642b3c0f650
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-12-07 19:37:05 -0300

    Adicionado reset e estrutura de rotas no projeto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
+
+import Reset from "./styles/Reset";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
+        <BrowserRouter>
+            <Reset></Reset>
+            <Routes>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
