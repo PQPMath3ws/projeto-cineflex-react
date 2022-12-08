6168c9bf13abfe289fc61cbd2cfd0b0231ab0b2b
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-12-08 12:05:42 -0300

    Correção do layout das images dos filmes

--- src/styles/MovieStyle.js
+++ src/styles/MovieStyle.js
@@ -9,6 +9,10 @@ const MovieStyle = {
     `,
     MovieImage: styled.img`
         width: 100%;
+        padding: 8px;
+        background-color: #FFFFFF;
+        box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+        border-radius: 3px;
     `
 };
 
