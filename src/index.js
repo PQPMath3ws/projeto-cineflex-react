019a4f355f9c21de2a3450fc251e979c2a1e4501
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-12-08 12:04:07 -0300

    Inserção de estilo global

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import reportWebVitals from './reportWebVitals';
 
-import Movies from "./routes/Movies";
 import Navbar from './components/Navbar';
+
+import Movies from "./routes/Movies";
+
+import Global from "./styles/Global";
 import Reset from "./styles/Reset";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,6 +17,7 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Reset></Reset>
+            <Global></Global>
             <Navbar></Navbar>
             <Routes>
                 <Route path="/" element={<Movies></Movies>}></Route>
