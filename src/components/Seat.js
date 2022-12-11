d8471b9378cd3968e55d178c96ba067319dfd491
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-12-11 20:25:51 -0300

    Correção de bugs e funcionalidades exigidas

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -9,12 +9,10 @@ const Seat = (props) => {
             let seats = [...props.selectedSeats];
             if (selected) {
                 let seat = seats.filter(seatToFilter => seatToFilter.seatId === props.id)[0];
-                if (seat.userName.length > 0 || seat.userCpf.length > 0) {
-                    if (window.confirm("Deseja realmente remover essa poltrona cadastrada?")) {
-                        let indexOf = seats.indexOf(seat);
-                        if (indexOf > -1) seats.splice(indexOf, 1);
-                        setSelected(false);
-                    }
+                if (window.confirm("Deseja realmente remover essa poltrona cadastrada?")) {
+                    let indexOf = seats.indexOf(seat);
+                    if (indexOf > -1) seats.splice(indexOf, 1);
+                    setSelected(false);
                 }
             } else {
                 seats.push({
@@ -26,11 +24,13 @@ const Seat = (props) => {
                 setSelected(true);
             }
             props.setSelectedSeats(seats);
+        } else {
+            alert("Esse assento está indisponível no momento.");
         }
     }
 
     return (
-        <SeatStyle.SeatDiv isAvailable={props.isAvailable} isSelected={selected} onClick={props.isAvailable ? changeSeatState : undefined} data-test="seat">
+        <SeatStyle.SeatDiv isAvailable={props.isAvailable} isSelected={selected} onClick={changeSeatState} data-test="seat">
             <SeatStyle.SeatNumber>{("0" + props.seatNumber).slice(-2)}</SeatStyle.SeatNumber>
         </SeatStyle.SeatDiv>
     );
